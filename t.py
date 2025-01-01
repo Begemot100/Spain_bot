27fa94fb63ce7de1a0c65d5e500d7c04f5cf9435
Begemot100 <you@example.com>
2025-01-01 06:07:28 +0100

    First commit with new project

---
+++ t.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(
+        dbname="railway",
+        user="postgres",
+        password="SHHgqKYlcgrgCsmptYWzhMNhgoKhyZtU",
+        host="roundhouse.proxy.rlwy.net",
+        port="42541"
+    )
+    print("Успешное подключение к базе данных!")
+    conn.close()
+except Exception as e:
+    print("Ошибка подключения к базе данных:", e)
